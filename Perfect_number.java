62d656130ce2adaa39c8110d1f928b753f6d9968
AnshikaAgarwal123 <Agarwalanshika246@gmail.com>
2026-03-17 21:25:46 +0530

    Create Perfect_number.java

---
+++ Perfect_number.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Perfect_number {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num= sc.nextInt();
+        int sum=0;
+        for(int i=1; i<=num/2; i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(sum==num){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
