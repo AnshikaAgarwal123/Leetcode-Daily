bf026a4e604def7f1bcdcffab6bf00308ba0859d
AnshikaAgarwal123 <Agarwalanshika246@gmail.com>
2026-04-22 23:05:43 +0530

    Create Search_insert_position.java

---
+++ Search_insert_position.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Search_insert_position{
+     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int nums[]= new int[n];
+        for(int i=0; i<n; i++){
+            nums[i]= sc.nextInt();
+        }     
+        int target= sc.nextInt(); 
+        int index=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]<target){
+                index=i+1;
+            }
+        }
+        System.out.println(index);
+    }
+}
