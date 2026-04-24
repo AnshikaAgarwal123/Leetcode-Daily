552214d0be862b97e601e51f1ebd37ac32632bd1
AnshikaAgarwal123 <Agarwalanshika246@gmail.com>
2026-04-24 23:52:16 +0530

    Create Max_achievable_no.java

---
+++ Max_achievable_no.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Max_achievable_no {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num=sc.nextInt();
+        int t=sc.nextInt();
+        System.out.println(num+2*t);
+    }
+}
+
