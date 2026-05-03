f023c3dbff7e6030735198a80f4fbc8f5951fec9
AnshikaAgarwal123 <Agarwalanshika246@gmail.com>
2026-05-03 23:19:53 +0530

    Create Multiply_strings.java

---
+++ Multiply_strings.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.*;
+class Multiply_strings {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String num1= sc.next();
+        String num2= sc.next();
+        BigInteger n1= new BigInteger(num1);
+        BigInteger n2= new BigInteger(num2);
+        BigInteger result= n1.multiply(n2);
+        System.out.println(result);
+    }
+}
