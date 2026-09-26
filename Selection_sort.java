52a3c57ab056003a3a2ab1346184d42372a59813
Anshika Agarwal <Agarwalanshika246@gmail.com>
2026-09-26 22:47:06 +0530

    Create Selection_sort.java

---
+++ Selection_sort.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Selection_sort {
+    public static void main(String[] args) {
+        int arr[]= {5, 4, 6, 3, 8, 5};
+        int n = arr.length;
+        for(int i=0; i<n-1; i++){
+            int min=i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+                int temp= arr[min];
+                arr[min]= arr[i];
+                arr[i]= temp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
